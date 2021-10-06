15afaa13bfcaa882ac9a60ec6eb3289fc710224b
gus <gusmarchand@gmail.com>
2021-10-06 12:22:39 +0200

    infinite scroll

--- client/src/actions/post.actions.js
+++ client/src/actions/post.actions.js
@@ -6,14 +6,16 @@ export const GET_POSTS = "GET_POSTS";
 export const LIKE_POST = "LIKE_POST";
 export const UNLIKE_POST = "UNLIKE_POST";
 
-export const getPosts = () => {
+export const getPosts = (num) => {
   return (dispatch) => {
     return axios({
       method: "GET",
       url: `${process.env.REACT_APP_API_URL}api/post/`,
     })
       .then((res) => {
-        dispatch({ type: GET_POSTS, playload: res.data });
+        const array = res.data.slice(0, num);
+
+        dispatch({ type: GET_POSTS, playload: array });
       })
       .catch((error) => {
         console.error(error);
